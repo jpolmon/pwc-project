c45246cf9b2582605abae88c68c62a7f27878840
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-12-15 17:18:59 -0600

    Buttons are now funcitonal, still need to be tied into the app.js

--- sample.js
+++ sample.js
@@ -6,22 +6,34 @@ const descriptionBox = document.getElementById('description');
 function roundsInfo() {
   descriptionBox.innerText = "Keep answering questions forever and see your average time per round.";
   rounds.classList.remove('is-light', 'is-outlined');
-  countdown.classList.remove('is-outlined');
-  casual.classList.remove('is-outlined');
+  if (!countdown.classList.contains('is-light','is-outlined')) {
+    countdown.classList.add('is-light', 'is-outlined');
+  }
+  if (!casual.classList.contains('is-light', 'is-outlined')) {
+    casual.classList.add('is-light', 'is-outlined');
+  }
 }
 
 function countdownInfo() {
   descriptionBox.innerText = "See how fast you can correctly answer 10 questions.";
-  rounds.classList.remove('is-outlined', 'is-light');
-  countdown.classList.toggle('is-outlined');
-  casual.classList.remove('is-outlined');
+  countdown.classList.remove('is-light', 'is-outlined');
+  if (!rounds.classList.contains('is-light','is-outlined')) {
+    rounds.classList.add('is-light', 'is-outlined');
+  }
+  if (!casual.classList.contains('is-light', 'is-outlined')) {
+    casual.classList.add('is-light', 'is-outlined');
+  }
 }
 
 function casualInfo() {
   descriptionBox.innerText = "See how many questions you can correctly answer in one minute.";
-  rounds.classList.remove('is-outlined');
-  countdown.classList.remove('is-outlined');
-  casual.classList.add('is-outlined');
+  casual.classList.remove('is-light', 'is-outlined');
+  if (!rounds.classList.contains('is-light','is-outlined')) {
+    rounds.classList.add('is-light', 'is-outlined');
+  }
+  if (!countdown.classList.contains('is-light', 'is-outlined')) {
+    countdown.classList.add('is-light', 'is-outlined');
+  }
 }
   
 rounds.addEventListener('click', roundsInfo);
