c9ba1c0e016f8eb0108ecfa61d70ecf20adf45ad
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-12-15 18:23:33 -0600

    Added nav bar and start button, finished MVP for general layout.

--- sample.js
+++ sample.js
@@ -1,10 +1,11 @@
 const rounds = document.getElementById('roundsBtn');
 const countdown = document.getElementById('countdownBtn');
 const casual = document.getElementById('casualBtn');
+const start = document.getElementById('startBtn');
 const descriptionBox = document.getElementById('description');
 
 function roundsInfo() {
-  descriptionBox.innerText = "Keep answering questions forever and see your average time per round.";
+  descriptionBox.innerText = "See how fast you can correctly answer 10 questions.";
   rounds.classList.remove('is-light', 'is-outlined');
   if (!countdown.classList.contains('is-light','is-outlined')) {
     countdown.classList.add('is-light', 'is-outlined');
@@ -12,10 +13,11 @@ function roundsInfo() {
   if (!casual.classList.contains('is-light', 'is-outlined')) {
     casual.classList.add('is-light', 'is-outlined');
   }
+  start.disabled = false;
 }
 
 function countdownInfo() {
-  descriptionBox.innerText = "See how fast you can correctly answer 10 questions.";
+  descriptionBox.innerText = "See how many questions you can correctly answer in one minute.";
   countdown.classList.remove('is-light', 'is-outlined');
   if (!rounds.classList.contains('is-light','is-outlined')) {
     rounds.classList.add('is-light', 'is-outlined');
@@ -23,10 +25,11 @@ function countdownInfo() {
   if (!casual.classList.contains('is-light', 'is-outlined')) {
     casual.classList.add('is-light', 'is-outlined');
   }
+  start.disabled = false;
 }
 
 function casualInfo() {
-  descriptionBox.innerText = "See how many questions you can correctly answer in one minute.";
+  descriptionBox.innerText = "Keep answering questions forever and see your average time per round.";
   casual.classList.remove('is-light', 'is-outlined');
   if (!rounds.classList.contains('is-light','is-outlined')) {
     rounds.classList.add('is-light', 'is-outlined');
@@ -34,8 +37,14 @@ function casualInfo() {
   if (!countdown.classList.contains('is-light', 'is-outlined')) {
     countdown.classList.add('is-light', 'is-outlined');
   }
+  start.disabled = false;
+}
+
+function startGame() {
+  start.style.display = 'none'
 }
   
 rounds.addEventListener('click', roundsInfo);
 countdown.addEventListener('click', countdownInfo);
 casual.addEventListener('click', casualInfo);
+start.addEventListener('click', startGame)
