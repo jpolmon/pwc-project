af80babca19c82013b99808c3a3f5310c4d47596
Jonathan Olmon <jonathan.olmon@gmail.com>
2022-01-17 14:52:51 -0600

    Finished the animations.

--- sample.js
+++ sample.js
@@ -1,17 +1,17 @@
 // Getting all required html elements
-const scoreboard = document.getElementById('scoreboardBtn');
-const rounds = document.getElementById('roundsBtn');
-const countdown = document.getElementById('countdownBtn');
-const casual = document.getElementById('casualBtn');
-const start = document.getElementById('startBtn');
+const scoreboard = document.getElementById("scoreboardBtn");
+const rounds = document.getElementById("roundsBtn");
+const countdown = document.getElementById("countdownBtn");
+const casual = document.getElementById("casualBtn");
+const start = document.getElementById("startBtn");
 
-const scoreboardModal = document.getElementById('scoreboard');
-const scoreboardBackground = document.getElementById('scoreboardBackground');
-const scoreboardClose = document.getElementById('scoreboardClose');
-const descriptionBox = document.getElementById('description');
-const resultBox = document.getElementById('resultBox');
-const gameInfoBox = document.getElementById('gameStats');
-const hiddenAtStart = document.querySelectorAll('.hiddenAtStart');
+const scoreboardModal = document.getElementById("scoreboard");
+const scoreboardBackground = document.getElementById("scoreboardBackground");
+const scoreboardClose = document.getElementById("scoreboardClose");
+const descriptionBox = document.getElementById("description");
+const resultBox = document.getElementById("resultBox");
+const gameInfoBox = document.getElementById("gameStats");
+const hiddenAtStart = document.querySelectorAll(".hiddenAtStart");
 
 if (!descriptionBox.innerText === "") {
   descriptionBox.style.display = "block";
@@ -21,24 +21,25 @@ if (!descriptionBox.innerText === "") {
 
 // Opens the scoreboard modal
 function renderScoreboard() {
-  scoreboardModal.classList.add('is-active');
+  scoreboardModal.classList.add("is-active");
 }
 
 // Closes the modal for the scoreboard
 function closeScoreboard() {
-  scoreboardModal.classList.remove('is-active');
+  scoreboardModal.classList.remove("is-active");
 }
 
 function roundsInfo() {
   // Adds the proper description
-  descriptionBox.innerText = "See how fast you can correctly answer 10 questions.";
+  descriptionBox.innerText =
+    "See how fast you can correctly answer 10 questions.";
   // Formats the buttons so the selected button is visually different from the others
-  rounds.classList.remove('is-light', 'is-outlined');
-  if (!countdown.classList.contains('is-light','is-outlined')) {
-    countdown.classList.add('is-light', 'is-outlined');
+  rounds.classList.remove("is-light", "is-outlined");
+  if (!countdown.classList.contains("is-light", "is-outlined")) {
+    countdown.classList.add("is-light", "is-outlined");
   }
-  if (!casual.classList.contains('is-light', 'is-outlined')) {
-    casual.classList.add('is-light', 'is-outlined');
+  if (!casual.classList.contains("is-light", "is-outlined")) {
+    casual.classList.add("is-light", "is-outlined");
   }
   // Enables the start button to be pressed
   start.disabled = false;
@@ -50,14 +51,15 @@ function roundsInfo() {
 
 function countdownInfo() {
   // Adds the proper description
-  descriptionBox.innerText = "See how many questions you can correctly answer in one minute.";
+  descriptionBox.innerText =
+    "See how many questions you can correctly answer in one minute.";
   // Formats the buttons so the selected button is visually different from the others
-  countdown.classList.remove('is-light', 'is-outlined');
-  if (!rounds.classList.contains('is-light','is-outlined')) {
-    rounds.classList.add('is-light', 'is-outlined');
+  countdown.classList.remove("is-light", "is-outlined");
+  if (!rounds.classList.contains("is-light", "is-outlined")) {
+    rounds.classList.add("is-light", "is-outlined");
   }
-  if (!casual.classList.contains('is-light', 'is-outlined')) {
-    casual.classList.add('is-light', 'is-outlined');
+  if (!casual.classList.contains("is-light", "is-outlined")) {
+    casual.classList.add("is-light", "is-outlined");
   }
   // Enables the start button to be pressed
   start.disabled = false;
@@ -70,14 +72,15 @@ function countdownInfo() {
 // Called when the casual mode is selected
 function casualInfo() {
   // Adds the proper description
-  descriptionBox.innerText = "Keep answering questions forever and see your average time per round.";
+  descriptionBox.innerText =
+    "Keep answering questions forever and see your average time per round.";
   // Formats the buttons so the selected button is visually different from the others
-  casual.classList.remove('is-light', 'is-outlined');
-  if (!rounds.classList.contains('is-light','is-outlined')) {
-    rounds.classList.add('is-light', 'is-outlined');
+  casual.classList.remove("is-light", "is-outlined");
+  if (!rounds.classList.contains("is-light", "is-outlined")) {
+    rounds.classList.add("is-light", "is-outlined");
   }
-  if (!countdown.classList.contains('is-light', 'is-outlined')) {
-    countdown.classList.add('is-light', 'is-outlined');
+  if (!countdown.classList.contains("is-light", "is-outlined")) {
+    countdown.classList.add("is-light", "is-outlined");
   }
   // Enables the start button to be pressed
   start.disabled = false;
@@ -89,17 +92,17 @@ function casualInfo() {
 
 // Function that will run when the start button is clicked, right now it just removes it from the screen.
 function startGame() {
-  start.style.display = 'none';
+  start.style.display = "none";
   for (const el of hiddenAtStart) {
-    el.classList.remove('hidden');
+    el.classList.remove("hidden");
   }
 }
 
 // Event listeners for all buttons
-scoreboard.addEventListener('click', renderScoreboard);
-scoreboardBackground.addEventListener('click', closeScoreboard);
-scoreboardClose.addEventListener('click', closeScoreboard);
-rounds.addEventListener('click', roundsInfo);
-countdown.addEventListener('click', countdownInfo);
-casual.addEventListener('click', casualInfo);
-start.addEventListener('click', startGame)
+scoreboard.addEventListener("click", renderScoreboard);
+scoreboardBackground.addEventListener("click", closeScoreboard);
+scoreboardClose.addEventListener("click", closeScoreboard);
+rounds.addEventListener("click", roundsInfo);
+countdown.addEventListener("click", countdownInfo);
+casual.addEventListener("click", casualInfo);
+start.addEventListener("click", startGame);
